d19f40744aff8ab66410f037a2a6b50fe770150a
Farhan Hoque <fhrubik98@gmail.com>
2025-10-01 16:29:29 +0600

    dashboard design complete

--- src/component/Dashboard/DashBoard.jsx
+++ src/component/Dashboard/DashBoard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import styles from './DashBoard.module.css'
+import ScoreIcon from '@mui/icons-material/Score';
+import Skeleton  from '@mui/material/Skeleton';
 
 export const DashBoard = () => {
   return (
@@ -41,6 +43,36 @@ export const DashBoard = () => {
 
             <div className={styles.dashboardAnalyzeButton}>Analyze</div>
           </div>
+
+      </div>
+
+      <div className={styles.DashBoardRight}>
+
+        <div className={styles.DashBoardRightTopCard}>
+
+          <div>Analyze with AI</div>
+
+          <img className={styles.profileIMG} src="https://randomuser.me/api/portraits/men/85.jpg" alt="Profile Photo" />
+
+          <h2>User Name</h2>
+        </div>
+
+        {/* <div className={styles.DashBoardRightTopCard}>
+
+          <div>RESULT</div>
+
+          <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '20px'}}>
+            <h1>75%</h1>
+            <ScoreIcon style={{fontSize: '30px'}} />
+          </div>
+
+          <div className={styles.dashboardFeedback}>
+            <h1>Feedback</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur repellat tenetur ipsum, officia animi et eaque incidunt tempora nisi placeat quam cum fugiat corporis id assumenda deleniti non eligendi corrupti!</p>
+          </div>
+        </div> */}
+
+        <Skeleton variant="rectangular" width={280} height={280} style={{ borderRadius: '20px' }} />
 
       </div>
 
